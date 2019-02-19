a4ccf77cae5a36a41608952d124e6a60c5d7ab8a
CHOUTINGMAN <43173983+CHOUTINGMAN@users.noreply.github.com>
2019-02-19 20:53:19 +0800

    Add files via upload

---
+++ 1N/w01-1-Using_the Console/app.js
@@ -0,0 +1,31 @@
+console.log('Hellow World');
+console.log(207410597); //ID
+console.log(true);
+
+var greeting ='周廷蔓';  //name
+console.log(greeting);
+console.log([2019,02,19]);  //today date
+console.log({firstname:'周',lastname:'廷蔓'}); //object
+console.table({firstname:'周',lastname:'廷蔓'});
+
+console.error('this is some error');
+console.warn('this is some warning');
+
+console.time('Hellow');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.log('Hellow World');
+console.timeEnd('Hellow');
+/*
+
+multi
+line
+comment
+
+*/
---
+++ 1N/w01-2-Variable-var-let-const/app.js
@@ -0,0 +1,51 @@
+//var , let , const
+
+var name ="Tingman Chou";
+console.log(name);
+name = '周廷蔓';
+console.log(name);
+name ="2019";
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = 'Hellow';
+console.log(greeting);
+
+//letter , numbers ,_, $,
+//annot start with number
+
+//multi word vars
+
+var firstName ="Tingman";//Camel case, preferred
+var first_name ="John"; //Underscore
+var FirstName ="Sare";//Pascal case
+var firstname;
+
+//let
+let name2 ="Tingman Chou";
+console.log(name2);
+name = '周廷蔓';
+console.log(name2);
+name ="2019";
+console.log(name2);
+
+//const
+//const name3="Tingman Chou";
+//console.log(name3);
+//name = '周廷蔓';
+//console.log(name3);
+//name ="2019";
+//console.log(name3);
+
+const person={
+    name:'tmc',
+    age:18
+};
+person.name = 'Choutingman';
+person.age =18;
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
